2b074989a95a2097b910aa15cbfd39c2ca5e964e
in1dm3-d <in1dm3-d@elorrieta.int>
2024-02-02 08:47:51 +0100

    Sarrera eta zinema test

--- Erronka3Prog/src/SarreraTest.java
+++ Erronka3Prog/src/SarreraTest.java
@@ -3,42 +3,38 @@ import org.junit.Test;
 public class SarreraTest {
   @Test
   public void testEquals() {
-      Sarrera sarrera1 = new Sarrera(1, 20220130, "avatar", 1200, 1, 10.5f);
-      Sarrera sarrera2 = new Sarrera(1, 20220130, "avatar", 1200, 1, 10.5f);
-      Sarrera sarrera3 = new Sarrera(2, 20220130, "harry potter", 1300, 2, 15.0f);
+      Sarrera sarrera1 = new Sarrera(1, 10, 5.0f,1);
+      Sarrera sarrera2 = new Sarrera(1, 10, 5.0f,1);
+      Sarrera sarrera3 = new Sarrera(2, 11, 5.0f,2);
       assertTrue(sarrera1.equals(sarrera2));
       assertFalse(sarrera1.equals(sarrera3));
   }
   @Test
   public void testHashCode() {
-      Sarrera sarrera1 = new Sarrera(1, 20220130, "avatar", 1200, 1, 10.5f);
-      Sarrera sarrera2 = new Sarrera(1, 20220130, "avatar", 1200, 1, 10.5f);
-      Sarrera sarrera3 = new Sarrera(2, 20220130, "harry potter", 1300, 2, 15.0f);
+	  Sarrera sarrera1 = new Sarrera(1, 10, 5.0f,1);
+      Sarrera sarrera2 = new Sarrera(1, 10, 5.0f,1);
+      Sarrera sarrera3 = new Sarrera(2, 11, 5.0f,2);
     
       assertEquals(sarrera1.hashCode(), sarrera2.hashCode());
       assertNotEquals(sarrera1.hashCode(), sarrera3.hashCode());
   }
   @Test
   public void testToString() {
-      Sarrera sarrera = new Sarrera(1, 20220130, "avatar", 1200, 1, 10.5f);
+      Sarrera sarrera = new Sarrera(1, 10, 5.0f,1);
      
-      assertEquals("Sarrera [id=1, data=20220130, ordutegia=1200, aretoa=1, prezioa=10.5]", sarrera.toString());
+      assertEquals("Sarrera [id=1, saioa=10, prezioa=5.0, erosketa=1]", sarrera.toString());
   }
  
   @Test
   public void testGettersAndSetters() {
       Sarrera sarrera = new Sarrera();
       sarrera.setId(1);
-      sarrera.setData(20220130);
-      sarrera.setFilma("avatar");
-      sarrera.setOrdutegia(1200);
-      sarrera.setAretoa(1);
-      sarrera.setPrezioa(10.5f);
+      sarrera.setSaioa(10);
+      sarrera.setPrezioa(5.0f);
+      sarrera.setErosketa(1);
       assertEquals(1, sarrera.getId());
-      assertEquals(20220130, sarrera.getData());
-      assertEquals("avatar", sarrera.getFilma());
-      assertEquals(1200, sarrera.getOrdutegia());
-      assertEquals(1, sarrera.getAretoa());
-      assertEquals(10.5f, sarrera.getPrezioa(), 0.001);
+      assertEquals(10, sarrera.getSaioa());
+      assertEquals(5.0f, sarrera.getPrezioa(), 0.001);
+      assertEquals(1, sarrera.getErosketa());
   }
 }
