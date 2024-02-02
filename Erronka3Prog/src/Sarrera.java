9c050429e833399551a140f42cd289e056df85c5
in1dm3-d <in1dm3-d@elorrieta.int>
2024-02-02 08:26:13 +0100

    sarrera zuzenduta

--- Erronka3Prog/src/Sarrera.java
+++ Erronka3Prog/src/Sarrera.java
@@ -5,34 +5,55 @@ public class Sarrera {
 	private int saioa;
 	private float prezioa;
 	private int erosketa;
+	private int sarrerakop;
+	
+	
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public int getSaioa() {
 		return saioa;
 	}
+
 	public void setSaioa(int saioa) {
 		this.saioa = saioa;
 	}
+
 	public float getPrezioa() {
 		return prezioa;
 	}
+
 	public void setPrezioa(float prezioa) {
 		this.prezioa = prezioa;
 	}
+
 	public int getErosketa() {
 		return erosketa;
 	}
+
 	public void setErosketa(int erosketa) {
 		this.erosketa = erosketa;
 	}
+
+	public int getSarrerakop() {
+		return sarrerakop;
+	}
+
+	public void setSarrerakop(int sarrerakop) {
+		this.sarrerakop = sarrerakop;
+	}
+
+	
 	@Override
 	public int hashCode() {
-		return Objects.hash(erosketa, id, prezioa, saioa);
+		return Objects.hash(erosketa, id, prezioa, saioa, sarrerakop);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -43,20 +64,25 @@ public class Sarrera {
 			return false;
 		Sarrera other = (Sarrera) obj;
 		return erosketa == other.erosketa && id == other.id
-				&& Float.floatToIntBits(prezioa) == Float.floatToIntBits(other.prezioa) && saioa == other.saioa;
+				&& Float.floatToIntBits(prezioa) == Float.floatToIntBits(other.prezioa) && saioa == other.saioa
+				&& sarrerakop == other.sarrerakop;
 	}
+
+	
 	@Override
 	public String toString() {
-		return "Sarrera [id=" + id + ", saioa=" + saioa + ", prezioa=" + prezioa + ", erosketa=" + erosketa + "]";
+		return "Sarrera [id=" + id + ", saioa=" + saioa + ", prezioa=" + prezioa + ", erosketa=" + erosketa
+				+ ", sarrerakop=" + sarrerakop + "]";
 	}
+
 	public Sarrera(int id, int saioa, float prezioa, int erosketa) {
 		this.id = id;
 		this.saioa = saioa;
 		this.prezioa = prezioa;
 		this.erosketa = erosketa;
+		this.sarrerakop = sarrerakop;
 	}
-	public Sarrera() {
-	}
+	
 	
 	
 }
