a540b654ce42bf05696bb3993e60e674c853a939
in1dm3-d <in1dm3-d@elorrieta.int>
2024-02-09 12:52:59 +0100

    Filma aldagaiak eginda

--- Erronka3Prog/src/Filma.java
+++ Erronka3Prog/src/Filma.java
@@ -1,4 +1,6 @@
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Objects;
 
 public class Filma {
@@ -14,8 +16,7 @@ public class Filma {
         
         
         
-		public Filma(int id, int iraupena, String generoa, double prezioa, String izena, String zuzendaria,
-				String sinopsia) {
+		public Filma() {
 			
 			this.id = id;
 			this.iraupena = iraupena;
@@ -107,5 +108,43 @@ public class Filma {
 			return "Filma [id=" + id + ", iraupena=" + iraupena + ", generoa=" + generoa + ", prezioa=" + prezioa
 					+ ", izena=" + izena + ", zuzendaria=" + zuzendaria + ", sinopsia=" + sinopsia + "]";
 		}
-		 
+		public Filma[] load(Connection conexioa) { 
+			 
+			 Filma[] filmak = null;;
+			 try {
+		    	   conexioa = konexioa.hasi();
+		           System.out.println("Konektatuta");
+		           // Prestatu sententzia
+		           int i = 0;
+		           Statement s1 = conexioa.createStatement();
+		           String sql = "select * from filma";
+		           ResultSet lerroak = s1.executeQuery(sql);
+		           int count = 0;
+		           while (lerroak.next()) {
+		        	   count++;
+		           }
+		           filmak =  new Filma[count];
+		           lerroak = s1.executeQuery(sql);
+		           while (lerroak.next()) {
+		               System.out.println("a");
+		               Filma filma = new Filma();
+		               filma.setId(lerroak.getInt("Idfilma"));
+		               filma.setIzena(lerroak.getString("Izenburua"));
+		               filma.setIraupena(lerroak.getInt("Iraupena")); 
+		               filma.setGeneroa(lerroak.getString("Generoa"));
+		               filma.setPrezioa(lerroak.getDouble("Prezioa"));
+		               filma.setZuzendaria(lerroak.getString("Zuzendaria"));
+		               filma.setSinopsia(lerroak.getString("Sinopsia"));
+		               filmak[i] = filma;
+		               i++;
+		           }
+		       } catch (Exception sqe) {
+		           sqe.printStackTrace();
+		       }
+		       for (int j = 0; j < filmak.length; j++) {
+		           System.out.println(filmak[j].toString());
+		       }
+		       return filmak;
+		   }
+
     }
