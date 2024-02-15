15fdd65e383fa686705768d588982134304999e6
in1dm3-d <in1dm3-d@elorrieta.int>
2024-02-15 12:55:17 +0100

    fullload sortuta

--- Erronka3Prog/src/master/Modelo.java
+++ Erronka3Prog/src/master/Modelo.java
@@ -15,11 +15,70 @@ public class Modelo {
 	Zinema Zinemak[];
 	
 	
-    public boolean irkeiLogin = false;
+	
+	
+    public Aretoa[] getAretoak() {
+		return Aretoak;
+	}
+
+	public void setAretoak(Aretoa[] aretoak) {
+		Aretoak = aretoak;
+	}
+
+	public Bezero[] getBezeroak() {
+		return Bezeroak;
+	}
+
+	public void setBezeroak(Bezero[] bezeroak) {
+		Bezeroak = bezeroak;
+	}
+
+	public Erosketa[] getErosketak() {
+		return Erosketak;
+	}
+
+	public void setErosketak(Erosketa[] erosketak) {
+		Erosketak = erosketak;
+	}
+
+	public Filma[] getFilmak() {
+		return Filmak;
+	}
+
+	public void setFilmak(Filma[] filmak) {
+		Filmak = filmak;
+	}
+
+	public Saioa[] getSaioak() {
+		return Saioak;
+	}
+
+	public void setSaioak(Saioa[] saioak) {
+		Saioak = saioak;
+	}
+
+	public Sarrera[] getSarrerak() {
+		return Sarrerak;
+	}
+
+	public void setSarrerak(Sarrera[] sarrerak) {
+		Sarrerak = sarrerak;
+	}
+
+	public Zinema[] getZinemak() {
+		return Zinemak;
+	}
+
+	public void setZinemak(Zinema[] zinemak) {
+		Zinemak = zinemak;
+	}
+
+	public boolean irkeiLogin = false;
 
     public Modelo() {
     }
 
+    
     public void abrirVetrorria() {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
