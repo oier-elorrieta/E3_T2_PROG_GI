1bdc8797c98d406f6dfce54fcb40dd98e2117ed5
in1dm3-d <in1dm3-d@elorrieta.int>
2024-02-20 08:57:22 +0100

    controlador osoa

--- Erronka3Prog/src/master/Controlador.java
+++ Erronka3Prog/src/master/Controlador.java
@@ -7,6 +7,30 @@ import vista.vLogin;
 public class Controlador {
 
     public static void main(String[] args) {
-    	
+    	System.out.println("HASI DA");
+        Modelo mDatuak = new Modelo();
+
+        konexioa k1 = new konexioa();
+        System.out.println("Konexioa burutuko da");
+        Connection saioa = k1.hasi();
+        System.out.println("Konexioa burutu da");
+        
+        k1.fullLoad(saioa);
+
+        vEtorria Ongipanel = new vEtorria(mDatuak);
+        Ongipanel.setVisible(true);
+         
+        while (!mDatuak.irekiLogin) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        Ongipanel.dispose();
+
+        vLogin Loginpanel = new vLogin(mDatuak);
+        Loginpanel.setVisible(true);
     }
 }
