daa26dcf2e96b5b646dd1e287a4d2002b9bc416c
hector.martineznu@elorrieta-errekamari.com <hector.martineznu@elorrieta-errekamari.com>
2024-02-20 11:31:44 +0100

    Beharrekoak ez diren funtzioak kendu

--- Erronka3Prog/src/master/Modelo.java
+++ Erronka3Prog/src/master/Modelo.java
@@ -16,18 +16,11 @@ import java.util.Date;
 import vista.vEtorria;
 
 public class Modelo {
-	//
-    Aretoa Aretoak[];
-    Bezero Bezeroak[];
-    Erosketa Erosketak[];
-    Filma Filmak[];
-    Saioa Saioak[];
-    Sarrera Sarrerak[];
-    //
     Zinema Zinemak[];
     public int azinema = 0;
     public int afilma = 0;
     public int asaioa = 0;
+    public boolean irekiLogin = false;
     
     public void zinemaproba() {
     	Aretoa aretoa2 = new Aretoa(1, "");
@@ -81,55 +74,7 @@ public class Modelo {
     }
     
 
-    public boolean irekiLogin = false;
     
-    public Aretoa[] getAretoak() {
-		return Aretoak;
-	}
-
-	public void setAretoak(Aretoa[] aretoak) {
-		Aretoak = aretoak;
-	}
-
-	public Bezero[] getBezeroak() {
-		return Bezeroak;
-	}
-
-	public void setBezeroak(Bezero[] bezeroak) {
-		Bezeroak = bezeroak;
-	}
-
-	public Erosketa[] getErosketak() {
-		return Erosketak;
-	}
-
-	public void setErosketak(Erosketa[] erosketak) {
-		Erosketak = erosketak;
-	}
-
-	public Filma[] getFilmak() {
-		return Filmak;
-	}
-
-	public void setFilmak(Filma[] filmak) {
-		Filmak = filmak;
-	}
-
-	public Saioa[] getSaioak() {
-		return Saioak;
-	}
-
-	public void setSaioak(Saioa[] saioak) {
-		Saioak = saioak;
-	}
-
-	public Sarrera[] getSarrerak() {
-		return Sarrerak;
-	}
-
-	public void setSarrerak(Sarrera[] sarrerak) {
-		Sarrerak = sarrerak;
-	}
 
 	public Zinema[] getZinemak() {
 		return Zinemak;
