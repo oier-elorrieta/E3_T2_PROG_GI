1710b63f7139f957a3b9f817218745f2a24cc887
in1dm3-d <in1dm3-d@elorrieta.int>
2024-02-23 10:10:58 +0100

    Saioa eta zinema testak eginda

--- Erronka3Prog/src/vista/vZinema.java
+++ Erronka3Prog/src/vista/vZinema.java
@@ -53,7 +53,7 @@ public class vZinema extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				int rowCount = model.getRowCount();
 				
-				Modelo.afilma = 0;
+				Modelo.setAfilma(0);
 				dispose();
 				 vLogin vLogin = new vLogin(Modelo);
 	             vLogin.setVisible(true);
@@ -70,8 +70,8 @@ public class vZinema extends JFrame {
 					int selectedRow = table.getSelectedRow();
 					if (selectedRow >= 0) {
 						JOptionPane.showMessageDialog(vZinema.this, table.getModel().getValueAt(selectedRow, 1).toString(), "Aukera", JOptionPane.INFORMATION_MESSAGE);
-						Modelo.azinema = selectedRow;
-						System.out.println(Modelo.azinema);
+						Modelo.setAzinema(selectedRow);
+						System.out.println(Modelo.getAzinema());
 					//hurrengo leioa "Filma" zabaldu behar da.
 						dispose();
 						// pasatu azinema bariablea gorde ahal izateko
